dabc324053f1b67fafa6eec1fe4647fc67c7107e
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2021-02-20 09:23:09 +0500

    Add files via upload

---
+++ Wrapper_class_Example.java
@@ -0,0 +1,15 @@
+public class Wrapper_class_Example {
+    public static void main(String[]args){
+
+        // Concerting to the Wrapper
+        Integer obj=new Integer(3);
+
+        // Convert from wrapper to simple by Explicity
+        int MyObject=obj.intValue();
+
+
+        System.out.println("\n\nFrom the Wrapper Class the value is "+obj);
+
+        System.out.println("From simple variable  is "+MyObject);
+    }
+}
